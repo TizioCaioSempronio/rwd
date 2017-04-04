f90835c1d3d0c1fef7f21207786a4644a4721737
TizioCaioSempronio <holly.era77@gmail.com>
2017-04-04 15:17:38 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var	lorem ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex eros, consequat tempus nunc quis, eleifend finibus ex. Quisque varius magna lectus, vitae tempor metus tempor sed. Phasellus at semper odio. Proin scelerisque ex fermentum ipsum ornare lobortis. Sed non est hendrerit ex aliquam gravida sed in dui. Praesent condimentum dolor a hendrerit porttitor. Etiam eget lacus fringilla, porttitor lorem et, placerat urna. Aliquam et volutpat risus, id feugiat ante. In venenatis sapien sed elit placerat rhoncus."
+lorem = lorem  +'<img src="images/sesto elemento.jpg">'+ lorem + lorem + lorem + lorem + lorem + '<img src="images/miura.jpg">'+ lorem + lorem + lorem + lorem 
+$ ('.lorem1').html(lorem);
+$ ('.lorem2').html(lorem);
+$ ('.lorem3').html(lorem);
+
